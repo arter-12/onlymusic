06e71e2f2a9fe46d9579e6383d8670710ccfa7f5
arter-12 <telegramotp000@gmail.com>
2025-05-17 14:37:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/codes0805/onlymusic",
+    "https://github.com/arter-12/onlymusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
